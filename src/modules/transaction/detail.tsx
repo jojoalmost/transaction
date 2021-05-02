c4434d6bc64ce51e993f0d3fe318f01381dea7c1
Yosua Risandy <yosua@majoo.id>
2021-05-03 01:46:14 +0700

    set value detail

--- src/modules/transaction/detail.tsx
+++ src/modules/transaction/detail.tsx
@@ -1,17 +1,36 @@
-import React, {FC} from "react";
+import React, {FC, useEffect, useState} from "react";
 import Button from "../../components/Button";
-import {useHistory} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import {ReactComponent as Harddrive} from "../../assets/harddrive.svg";
-import {toIdrCurrency} from "../../utils/helper";
+import {dateToLocaleString, toIdrCurrency} from "../../utils/helper";
+import {TransactionInterface, TransactionStatus} from "../../interfaces/transaction";
 
 const Detail: FC = props => {
+    const [transaction, setTransaction] = useState<TransactionInterface>({
+        account_number: '',
+        amount: 0,
+        beneficiary_bank: '',
+        beneficiary_name: '',
+        completed_at: '',
+        created_at: '',
+        fee: 0,
+        id: '',
+        remark: '',
+        sender_bank: '',
+        status: 'SUCCESS',
+        unique_code: 0,
+    });
     const history = useHistory();
+    // const {id} = useParams();
+    useEffect(() => {
+
+    }, []);
     return (
         <>
             <h1 className="title">Detail Transaksi</h1>
             <div className="card-detail-info" style={{alignItems: "center", marginBottom: 16}}>
                 <div className="text-uppercase" style={{flex: 1, fontWeight: 600}}>
-                    id transaksi: #FT456
+                    id transaksi: #{transaction?.id}
                 </div>
                 <div>
                     <Button status={"SUCCESS"}
@@ -24,30 +43,30 @@ const Detail: FC = props => {
                 <div>
                     <div className="detail-info">
                         <h5>Pengirim</h5>
-                        <div>BCA</div>
+                        <div>{transaction?.sender_bank}</div>
                     </div>
 
                     <div className="detail-info">
                         <h5>penerima</h5>
-                        <div className="text-capitalize">mandiri</div>
-                        <div>123456</div>
-                        <div>bambang</div>
+                        <div>{transaction?.beneficiary_bank}</div>
+                        <div>{transaction?.account_number}</div>
+                        <div>{transaction?.beneficiary_name}</div>
                     </div>
 
                     <div className="detail-info">
                         <h5>nominal</h5>
-                        <div>{toIdrCurrency(100000)}</div>
-                        <div>kode unik: 50</div>
+                        <div>{toIdrCurrency(transaction?.amount)}</div>
+                        <div>kode unik: {transaction?.unique_code}</div>
                     </div>
 
                     <div className="detail-info">
                         <h5>catatan</h5>
-                        <div>sebuah remark</div>
+                        <div>{transaction?.remark}</div>
                     </div>
 
                     <div className="detail-info">
                         <h5>waktu dibuat</h5>
-                        <div>1 januari 2020</div>
+                        <div>{dateToLocaleString(transaction?.created_at)}</div>
                     </div>
                 </div>
             </div>
